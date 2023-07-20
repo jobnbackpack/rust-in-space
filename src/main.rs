1fe806bc584367e76377e548a39913683fdad735
Robin Janzen <rjanzen@conet.de>
2023-07-20 08:20:48 +0200

    chore: implemented the example from leptos github

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,124 @@
-use leptos::*;
-use serde::Deserialize;
-use serde::Serialize;
-
-#[derive(Deserialize, Serialize, Debug, Clone)]
-struct User {
-    login: String,
-    id: u32,
-}
-
-#[tokio::main]
-async fn load_data() -> User {
-    let response =
-        reqwest::get("https://api.github.com/repos/rust-lang-nursery/rust-cookbook/stargazers")
-            .await
-            .unwrap()
-            .json::<Vec<User>>()
-            .await;
-
-    log!("{:?}", response);
-
-    User {
-        login: "test".to_string(),
-        id: 22,
-    }
-    // match response {
-    //     Ok(users) => {
-    //         log!("Processing user response {:?}", users);
-    //         return user.;
-    //     }
-    //     Err(e) => {
-    //         error!("Orders API response cannot be parsed! {}", e)
-    //     }
-    // }
-}
+use leptos::{error::Result, *};
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
 
 fn main() {
-    mount_to_body(|cx| view! { cx,  <App/> })
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+    mount_to_body(fetch_example)
 }
 
 #[component]
 fn App(cx: Scope) -> impl IntoView {
     view! {cx,
-        <p>"Hello"</p>
+        // <LaunchList/>
+        <p>"hello"</p>
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Cat {
+    url: String,
+}
+
+#[derive(Error, Clone, Debug)]
+pub enum CatError {
+    #[error("Please request more than zero cats.")]
+    NonZeroCats,
+}
+
+type CatCount = usize;
+
+async fn fetch_cats(count: CatCount) -> Result<Vec<String>> {
+    if count > 0 {
+        // make the request
+        let res = reqwasm::http::Request::get(&format!(
+            "https://api.thecatapi.com/v1/images/search?limit={count}",
+        ))
+        .send()
+        .await?
+        // convert it to JSON
+        .json::<Vec<Cat>>()
+        .await?
+        // extract the URL field for each cat
+        .into_iter()
+        .take(count)
+        .map(|cat| cat.url)
+        .collect::<Vec<_>>();
+        Ok(res)
+    } else {
+        Err(CatError::NonZeroCats.into())
+    }
+}
+
+pub fn fetch_example(cx: Scope) -> impl IntoView {
+    let (cat_count, set_cat_count) = create_signal::<CatCount>(cx, 0);
+
+    // we use local_resource here because
+    // 1) our error type isn't serializable/deserializable
+    // 2) we're not doing server-side rendering in this example anyway
+    //    (during SSR, create_resource will begin loading on the server and resolve on the client)
+    let cats = create_local_resource(cx, cat_count, fetch_cats);
+
+    let fallback = move |cx, errors: RwSignal<Errors>| {
+        let error_list = move || {
+            errors.with(|errors| {
+                errors
+                    .iter()
+                    .map(|(_, e)| view! { cx, <li>{e.to_string()}</li> })
+                    .collect_view(cx)
+            })
+        };
+
+        view! { cx,
+            <div class="error">
+                <h2>"Error"</h2>
+                <ul>{error_list}</ul>
+            </div>
+        }
+    };
+
+    // the renderer can handle Option<_> and Result<_> states
+    // by displaying nothing for None if the resource is still loading
+    // and by using the ErrorBoundary fallback to catch Err(_)
+    // so we'll just implement our happy path and let the framework handle the rest
+    let cats_view = move || {
+        cats.read(cx).map(|data| {
+            data.map(|data| {
+                data.iter()
+                    .map(|s| view! { cx, <p><img src={s}/></p> })
+                    .collect_view(cx)
+            })
+        })
+    };
+
+    view! { cx,
+        <div>
+            <label>
+                "How many cats would you like?"
+                <input
+                    type="number"
+                    prop:value=move || cat_count.get().to_string()
+                    on:input=move |ev| {
+                        let val = event_target_value(&ev).parse::<CatCount>().unwrap_or(0);
+                        set_cat_count(val);
+                    }
+                />
+            </label>
+            <ErrorBoundary fallback>
+                <Transition fallback=move || {
+                    view! { cx, <div>"Loading (Suspense Fallback)..."</div> }
+                }>
+                <div>
+                    {cats_view}
+                </div>
+                </Transition>
+            </ErrorBoundary>
+        </div>
     }
 }
 
 #[component]
 fn LaunchList(cx: Scope) -> impl IntoView {
-    let once = create_resource(cx, || (), |_| async move { load_data() });
-    view! { cx,
-        <h1>"My Data"</h1>
-        {move || match once.read(cx) {
-            None => view! { cx, <p>"Loading..."</p> }.into_view(cx),
-            // Some(data) => view! { cx, <ShowData data/> }.into_view(cx)
-             Some(data) => {
-                log!("{:?}", data);
-                view! { cx, <p>"Ready!"</p> }.into_view(cx)
-            }
-        }}
-    }
+    fetch_example(cx)
 }
