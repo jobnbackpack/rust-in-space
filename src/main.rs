eb48433ea4fc21e4d50d84c8e657c45bb0f73c1c
Robin Janzen <rjanzen@conet.de>
2023-07-21 13:07:32 +0200

    feat: introduce routing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use leptos::*;
+use leptos_router::*;
 
 use crate::launches::launches::LaunchList;
 
@@ -7,13 +8,39 @@ mod launches;
 fn main() {
     _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
-    mount_to_body(|cx| view! {cx, <div class="container"><App/></div>})
+    mount_to_body(|cx| {
+        view! {cx,
+                    <App/>
+        }
+    })
 }
 
 #[component]
 fn App(cx: Scope) -> impl IntoView {
     view! {cx,
-        <h1>"Rust-In-Space"</h1>
-        <LaunchList/>
+        <Router>
+            <div class="container">
+            <nav>
+                <ul>
+                    <li><strong>"Rust-In-Space"</strong></li>
+                </ul>
+                <ul>
+                    <li><A href="/">Home</A></li>
+                    <li><A href="/lol">Lol</A></li>
+                </ul>
+            </nav>
+            <Routes>
+                <Route path="/" view=LaunchList/>
+                <Route path="/lol" view=TestView/>
+            </Routes>
+            </div>
+        </Router>
+    }
+}
+
+#[component]
+fn TestView(cx: Scope) -> impl IntoView {
+    view! {cx,
+        <h3>lol</h3>
     }
 }
